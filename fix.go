672956041a6795310e5d0184aede558060294507
juntaki <me@juntaki.com>
2018-07-21 11:02:52 +0900

    init

---
+++ fix.go
@@ -0,0 +1,109 @@
+package fix
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/pkg/errors"
+)
+
+type Payload struct {
+	Target interface{}
+}
+
+func encode(target interface{}) ([]byte, error) {
+	gob.Register(target)
+
+	p := Payload{
+		Target: target,
+	}
+
+	buf := bytes.NewBuffer([]byte{})
+
+	enc := gob.NewEncoder(buf)
+	err := enc.Encode(&p)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func decode(raw []byte, target *interface{}) error {
+	p := Payload{}
+
+	dec := gob.NewDecoder(bytes.NewReader(raw))
+	err := dec.Decode(&p)
+	if err != nil {
+		return err
+	}
+	*target = p.Target
+	return nil
+}
+
+// DefaultOutputPath is ./testdata/<caller_func_name>
+func DefaultOutputPath(funcName string) string {
+	baseDir := "testdata/"
+	filename := strings.Replace(funcName, "/", "_", -1)
+	return filepath.Join(baseDir, filename)
+}
+
+var outputPath = DefaultOutputPath
+
+// SetOutputPathFunc overwrite DefaultOutputPath
+func SetOutputPathFunc(in func(funcName string) string) {
+	outputPath = in
+}
+
+// Fix target pointer as gob encoded data file.
+// if file doesn't exist, just write data to file and return error.
+// if file exist, test if the target is the same as file's data.
+func Fix(target interface{}) error {
+	pt, _, _, _ := runtime.Caller(1)
+	funcName := runtime.FuncForPC(pt).Name()
+	path := outputPath(funcName)
+
+	err := os.MkdirAll(filepath.Dir(path), 0777)
+	if err != nil {
+		return errors.Wrap(err, "Cannot make dir")
+	}
+
+	ret, err := encode(target)
+	if err != nil {
+		return errors.Wrap(err, "Target cannot encode")
+	}
+
+	_, err = os.Stat(path)
+	if err != nil { // file is not exist
+		ioutil.WriteFile(path, ret, 0666)
+		return errors.New("Write to file")
+	} else { // file exist
+		raw, err := ioutil.ReadFile(path)
+		if err != nil {
+			return errors.Wrap(err, "File cannot read: "+path)
+		}
+
+		// If equal, target still have the same result.
+		if bytes.Equal(ret, raw) {
+			return nil
+		}
+
+		// If not, try decode and show diff as error
+		var valid interface{}
+		err = decode(raw, &valid)
+		if err != nil {
+			return errors.Wrap(err, "File cannot decode: "+path)
+		}
+
+		return fmt.Errorf("Diff: %s", cmp.Diff(target, valid, cmpopts.EquateEmpty()))
+	}
+
+}
---
+++ fix_test.go
@@ -0,0 +1,128 @@
+package fix
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Test struct {
+	Sub TestSub
+}
+
+type TestSub struct {
+	Value string
+}
+
+func Test_encode(t *testing.T) {
+	type args struct {
+		target interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    interface{}
+		wantErr bool
+	}{
+		{
+			name: "",
+			args: args{
+				target: &Test{
+					Sub: TestSub{
+						Value: "test",
+					},
+				},
+			},
+			want: &Test{
+				Sub: TestSub{
+					Value: "test",
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			raw, err := encode(tt.args.target)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("encode() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			var got interface{}
+			if err := decode(raw, &got); (err != nil) != tt.wantErr {
+				t.Errorf("decode() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("encode/decode = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDefaultOutputPath(t *testing.T) {
+	type args struct {
+		funcName string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "base",
+			args: args{
+				funcName: "test",
+			},
+			want: "testdata/test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DefaultOutputPath(tt.args.funcName); got != tt.want {
+				t.Errorf("DefaultOutputPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSetOutputPathFunc(t *testing.T) {
+	SetOutputPathFunc(func(funcName string) string {
+		return funcName
+	})
+	type args struct {
+		funcName string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "base",
+			args: args{
+				funcName: "test",
+			},
+			want: "test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := outputPath(tt.args.funcName); got != tt.want {
+				t.Errorf("DefaultOutputPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFix(t *testing.T) {
+	SetOutputPathFunc(DefaultOutputPath)
+	test := &Test{
+		Sub: TestSub{
+			Value: "test",
+		},
+	}
+
+	err := Fix(test)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
